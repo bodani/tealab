4a810d32daf054b8dfd5e4a6659b307aa51c907f
ZhangEamon <EamonZhang>
2023-02-14 17:20:15 +0800

    add docker roles

---
+++ roles/monitor/files/load_dashboard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# grafana access info
+export GRAFANA_ENDPOINT="http://127.0.0.1:3000"
+export GRAFANA_USERNAME={{ grafana_admin_username }}
+export GRAFANA_PASSWORD={{ grafana_admin_password }}
+
+# run provisinoing logic
+python /var/lib/grafana/grafana.py init /var/lib/grafana/dashboards
