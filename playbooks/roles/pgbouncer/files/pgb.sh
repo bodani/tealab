7731c792e6d6a7370a3bb956330dfa4f619c9c70
EamonZhang <zhangeamon@hotmail.com>
2023-03-17 09:27:49 +0800

    add pgbouncer aliase

--- playbooks/roles/pgbouncer/files/pgb.sh
+++ playbooks/roles/pgbouncer/files/pgb.sh
@@ -1,2 +1,3 @@
 alias pgb_admin='psql -U tea_mon  -p 6432 -h 127.0.01  -d pgbouncer'
 alias pgb_stats='psql -U tea_mon  -p 6432 -h 127.0.01  -d pgbouncer'
+alias pgb_sync_users='python /etc/pgbouncer/mkauth.py /etc/pgbouncer/userlist.txt  "host=localhost user=postgres"'
